d5aa8a060ff941b57bfa7cbcb72d26563dd75a75
getsadzeg <getsadzeguram@gmail.com>
2018-09-16 13:25:58 +0400

    generate sample QR

--- app/src/main/java/ge/qrapp/activity/ReceiveActivity.java
+++ app/src/main/java/ge/qrapp/activity/ReceiveActivity.java
@@ -1,19 +1,29 @@
 package ge.qrapp.activity;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import net.glxn.qrgen.android.QRCode;
+import net.glxn.qrgen.core.image.ImageType;
+
+import java.io.File;
 
 import androidx.appcompat.app.AppCompatActivity;
 import ge.qrapp.R;
 
 public class ReceiveActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive);
-        /* File file = QRCode.from("Hello World").file();
-        QRCode.from("Hello World").withSize(250, 250).file();
-        QRCode.from("Hello World").withSize(250, 250).stream();
-        System.out.println(file.getAbsolutePath() + "   PATH"); */
+        imageView = findViewById(R.id.imageview);
+        File file = QRCode.from("Hello World").to(ImageType.JPG).file();
+        System.out.println("PATH " + file.getAbsolutePath());
+        imageView.setImageBitmap(BitmapFactory.decodeFile(file.getAbsolutePath()));
+        //imageView.getLayoutParams().width=120;
     }
 }
